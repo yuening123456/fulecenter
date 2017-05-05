70a5f33e7e869208c9e877a135c02bbd569f9328
yuening123456 <qw1007260923@qq.com>
2017-05-05 14:34:26 +0800

    新品首页的数据下载

--- app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter_2017.ui.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -8,25 +9,62 @@ import android.view.View;
 
 import cn.ucai.fulicenter_2017.R;
 
+import cn.ucai.fulicenter_2017.ui.fragment.BoutiqueFragment;
 import cn.ucai.fulicenter_2017.ui.fragment.NewGoodsFragment;
 
 public class MainActivity extends AppCompatActivity {
     NewGoodsFragment goodsFragment;
-
-
+    BoutiqueFragment boutiqueFragment;
+    Fragment[] mFragment;
+    int currentIndex, index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFragment();
+        showFragment();
+    }
+
+
+
+    private void initFragment() {
         goodsFragment=new NewGoodsFragment();
+        boutiqueFragment=new BoutiqueFragment();
+        mFragment=new Fragment[5];
+        mFragment[0]=goodsFragment;
+        mFragment[1]=boutiqueFragment;
+    }
+    private void showFragment() {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction ft = manager.beginTransaction();
-        ft.add(R.id.layoutFragment,goodsFragment).commit();
+        ft.add(R.id.layoutFragment,goodsFragment)
+                .add(R.id.layoutFragment,boutiqueFragment)
+                .show(goodsFragment)
+                .hide(boutiqueFragment)
+                .commit();
     }
+
     public  void onCheckedChange(View view){
+            switch (view.getId()){
+                case R.id.tvNewGoods:
+                    index=0;
+                    break;
+                case R.id.tvBoutique:
+                    index=1;
+                    break;
+            }
+            setFragment();
 
+    }
 
+    private void setFragment() {
+        if(index!=currentIndex){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.layoutFragment,mFragment[index]).commit();
+            currentIndex=index;
+
+        }
     }
 
 }
--- app/src/main/java/cn/ucai/fulicenter_2017/ui/fragment/BoutiqueFragment.java
+++ app/src/main/java/cn/ucai/fulicenter_2017/ui/fragment/BoutiqueFragment.java
@@ -3,30 +3,77 @@ package cn.ucai.fulicenter_2017.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter_2017.R;
-import cn.ucai.fulicenter_2017.application.I;
+import cn.ucai.fulicenter_2017.data.bean.BoutiqueBean;
+import cn.ucai.fulicenter_2017.data.net.GoodsModel;
+import cn.ucai.fulicenter_2017.data.net.IGoodsModel;
+import cn.ucai.fulicenter_2017.data.net.OnCompleteListener;
+import cn.ucai.fulicenter_2017.data.utils.L;
+import cn.ucai.fulicenter_2017.data.utils.ResultUtils;
+
+import static android.R.id.list;
 
 /**
  * Created by Administrator on 2017/5/5 0005.
  */
 
 public class BoutiqueFragment extends Fragment {
+    @BindView(R.id.tvDown)
+    TextView tvDown;
+    @BindView(R.id.rvGoods)
+    RecyclerView rvGoods;
+    @BindView(R.id.srfl)
+    SwipeRefreshLayout srfl;
+    @BindView(R.id.tv_nore)
+    TextView tvNore;
+    Unbinder unbinder;
+    IGoodsModel model;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new_goods, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        model=new GoodsModel();
+        loadData();
+    }
+    private void loadData() {
+        model.loadBoutiqueData(getContext(), new OnCompleteListener<BoutiqueBean[]>() {
+            @Override
+            public void onSuccess(BoutiqueBean[] result) {
+                L.e("main","result="+result.length);
+                ArrayList<BoutiqueBean> list = ResultUtils.array2List(result);
+
+            }
+            @Override
+            public void onError(String error) {
+                L.e("main","error="+error);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
