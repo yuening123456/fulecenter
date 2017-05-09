e640202fbd839f3db79ca8de696346139d0486f3
yuening123456 <qw1007260923@qq.com>
2017-05-09 10:50:00 +0800

    设置商品分类二级界面数据的显示

---
+++ app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/CategoryChildActivity.java
@@ -0,0 +1,52 @@
+package cn.ucai.fulicenter_2017.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import cn.ucai.fulicenter_2017.R;
+import cn.ucai.fulicenter_2017.application.I;
+import cn.ucai.fulicenter_2017.ui.fragment.NewGoodsFragment;
+
+public class CategoryChildActivity extends AppCompatActivity {
+
+    @BindView(R.id.backClickArea)
+    ImageView backClickArea;
+    @BindView(R.id.tv_common_title)
+    TextView tvCommonTitle;
+    @BindView(R.id.layoutFragment)
+    FrameLayout layoutFragment;
+    Unbinder bind;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_child);
+        bind=ButterKnife.bind(this);
+        int catId=getIntent().getIntExtra(I.CategoryChild.CAT_ID,I.CAT_ID);
+        String title=getIntent().getStringExtra(I.Boutique.TITLE);
+        tvCommonTitle.setText(title);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.layoutFragment,new NewGoodsFragment(catId))
+                .commit();
+    }
+    @OnClick(R.id.backClickArea)
+    public void onClick(View view){
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(bind!=null){
+            bind.unbind();
+        }
+    }
+}
