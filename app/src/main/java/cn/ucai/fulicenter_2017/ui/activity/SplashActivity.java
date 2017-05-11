37aadb5fdca114fbec71f418f64deab653376b39
yuening123456 <qw1007260923@qq.com>
2017-05-11 10:21:57 +0800

    闪屏界面拿到用户信息，

--- app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/SplashActivity.java
@@ -12,6 +12,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import cn.ucai.fulicenter_2017.R;
+import cn.ucai.fulicenter_2017.application.FuLiCenterApplication;
+import cn.ucai.fulicenter_2017.data.bean.User;
+import cn.ucai.fulicenter_2017.data.local.UserDao;
+import cn.ucai.fulicenter_2017.data.utils.L;
+import cn.ucai.fulicenter_2017.data.utils.SharePrefrenceUtils;
 
 public class SplashActivity extends AppCompatActivity {
     final static int time = 5000;
@@ -28,6 +33,30 @@ public class SplashActivity extends AppCompatActivity {
         countDownTimer.start();
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(FuLiCenterApplication.getInstance().getCurrentUser()==null){
+                    String userName= SharePrefrenceUtils.getInstance().getUserName();
+                    L.e("main","userName="+userName);
+                    if(userName!=null){
+                        UserDao dao=new UserDao(SplashActivity.this);
+                        User user=dao.getUser(userName);
+                        if(user!=null){
+                            FuLiCenterApplication.getInstance().setCurrentUser(user);
+                        }
+                    }
+
+                }
+            }
+        }).start();
+
+    }
+
     @OnClick(R.id.skip) void skip(){
         countDownTimer.cancel();
         countDownTimer.onFinish();
