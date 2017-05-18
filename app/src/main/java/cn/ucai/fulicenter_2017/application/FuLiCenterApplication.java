7a6ed8aaa9e150c07ac958689da46440f40d2aba
yuening123456 <qw1007260923@qq.com>
2017-05-18 15:19:54 +0800

    加入第三方框架，分享的实现

--- app/src/main/java/cn/ucai/fulicenter_2017/application/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter_2017/application/FuLiCenterApplication.java
@@ -2,6 +2,7 @@ package cn.ucai.fulicenter_2017.application;
 
 import android.app.Application;
 
+import cn.sharesdk.framework.ShareSDK;
 import cn.ucai.fulicenter_2017.data.bean.User;
 
 
@@ -17,6 +18,7 @@ public class FuLiCenterApplication extends Application{
     public void onCreate() {
         super.onCreate();
         instace=this;
+        ShareSDK.initSDK(this);
     }
 
     public static FuLiCenterApplication getInstance(){
--- app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/GoodDetailsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter_2017/ui/activity/GoodDetailsActivity.java
@@ -16,6 +16,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter_2017.R;
 import cn.ucai.fulicenter_2017.application.FuLiCenterApplication;
 import cn.ucai.fulicenter_2017.application.I;
@@ -70,6 +71,7 @@ public class GoodDetailsActivity extends AppCompatActivity {
     @BindView(R.id.shopping)
     TextView shopping;
     GoodsDetailsBean mGoodsDetailsBean;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,7 +108,7 @@ public class GoodDetailsActivity extends AppCompatActivity {
         model.loadGoodDetails(this, good_id, new OnCompleteListener<GoodsDetailsBean>() {
             @Override
             public void onSuccess(GoodsDetailsBean result) {
-                mGoodsDetailsBean=result;
+                mGoodsDetailsBean = result;
                 showView(result);
             }
 
@@ -231,7 +233,7 @@ public class GoodDetailsActivity extends AppCompatActivity {
                 public void onSuccess(MessageBean result) {
                     if (result != null && result.isSuccess()) {
                         CommonUtils.showLongToast(R.string.add_goods_success);
-                        sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART).putExtra(I.Cart.class.toString(),mGoodsDetailsBean));
+                        sendBroadcast(new Intent(I.BROADCAST_UPDATA_CART).putExtra(I.Cart.class.toString(), mGoodsDetailsBean));
                     } else {
                         CommonUtils.showLongToast(R.string.add_goods_fail);
                     }
@@ -247,4 +249,33 @@ public class GoodDetailsActivity extends AppCompatActivity {
         }
     }
 
+    @OnClick(R.id.iv_share)
+    public void onShare() {
+
+            OnekeyShare oks = new OnekeyShare();
+            //关闭sso授权
+            oks.disableSSOWhenAuthorize();
+            // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+            oks.setTitle("标题");
+            // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+            oks.setTitleUrl("http://sharesdk.cn");
+            // text是分享文本，所有平台都需要这个字段
+            oks.setText("我是分享文本");
+            //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+            oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+            //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            // url仅在微信（包括好友和朋友圈）中使用
+            oks.setUrl("http://sharesdk.cn");
+            // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+            oks.setComment("我是测试评论文本");
+            // site是分享此内容的网站名称，仅在QQ空间使用
+            oks.setSite("ShareSDK");
+            // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+            oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+            oks.show(this);
+
+    }
 }
